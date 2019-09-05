bb16b0159d23ebf93bba025f32e3d2857e7d1cb4
herman-11 <54630508+herman-11@users.noreply.github.com>
2019-09-05 20:56:44 +0800

    U-app.yml
    增加上传的大小

--- src/main/java/com/nkp/config/utils/Test.java
+++ src/main/java/com/nkp/config/utils/Test.java
@@ -19,11 +19,11 @@ public class Test {
     public String upload(@RequestParam("file") MultipartFile file){
 
         String fileName=file.getOriginalFilename();
-        //2上传失败提示
+
         String warning="";
         String newFileName = FileUtils.upload(file, path, fileName);
         if(newFileName != null){
-            //上传成功
+
             warning="上传成功";
 
         }else{
