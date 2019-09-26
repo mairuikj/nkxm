c99b57803203504f640e9874c1ca5132615134c0
hrx123 <785349471@qq.com>
2019-09-27 00:49:37 +0800

    更

--- src/main/resources/static/js/font.js
+++ src/main/resources/static/js/font.js
@@ -15,28 +15,3 @@
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
-
-function request(url,data,fn){
-    var baseUrl = ''
-    url = baseUrl + url
-    $.ajax({
-        url  : url,
-        type: 'post',
-        async: true,
-        dataType : "json",
-        data : data,
-        success: function(response){
-            if(response.code == "0"){
-                //请求成功后执行
-                fn(response.data)
-            }else{
-                //请求失败
-                alert("请求数据失败！")
-            }
-        },
-        error:function (err) {
-            alert('请求出错！')
-            console.log('请求出错:',err)
-        }
-    });
-}
