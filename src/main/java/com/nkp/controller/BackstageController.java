270baaa28b76b3f09545038c14a683448e09e0f6
herman-11 <54630508+herman-11@users.noreply.github.com>
2019-10-27 16:42:49 +0800

    短信登录

--- src/main/java/com/nkp/controller/BackstageController.java
+++ src/main/java/com/nkp/controller/BackstageController.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.nkp.config.utils.DataPackJSON;
 import com.nkp.dao.ShrioMapper;
+import com.nkp.dao.UserInfoMapper;
 import com.nkp.pojo.Shrio;
 import com.nkp.pojo.UserInfo;
 import com.nkp.service.UserService;
@@ -18,7 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -31,25 +35,41 @@ public class BackstageController {
 
     @Autowired
     private ShrioMapper shrioMapper;
+    @Autowired
+    private UserInfoMapper userInfoMapper;
 
-/*    public DataPackJSON backStage(String userName, String userPW, HttpServletRequest request){
-        UserInfo userInfo=userService.check(userName,userPW);
+    @RequestMapping("/check")
+    public DataPackJSON backStage(String phone, String code, HttpServletRequest request){
+        HttpSession session=request.getSession();
         DataPackJSON dataPackJSON=new DataPackJSON();
         Map map=new HashMap();
+        UserInfo userInfo=userInfoMapper.selectByPhone(phone);
         if(userInfo!=null){
-            request.getSession().setAttribute("session_user",userInfo);
-            map.put("session_user",(UserInfo)request.getSession().getAttribute("session_user"));
-            dataPackJSON.setFlag(0);
-            dataPackJSON.setMsg("SUCCESS");
-            dataPackJSON.setMap(map);
-            return dataPackJSON;
+            String sessionCode=session.getAttribute("code").toString();
+            if(sessionCode==null){
+                dataPackJSON.setFlag(1);
+                dataPackJSON.setMsg("验证码过期");
+                return dataPackJSON;
+            }
+            if(sessionCode.equals(code)){
+                session.setAttribute("session_user",userInfo);
+                map.put("session_user",(UserInfo)request.getSession().getAttribute("session_user"));
+                dataPackJSON.setFlag(0);
+                dataPackJSON.setMsg("SUCCESS");
+                dataPackJSON.setMap(map);
+                return dataPackJSON;
+            }else {
+                dataPackJSON.setFlag(1);
+                dataPackJSON.setMsg("验证码错误");
+                return dataPackJSON;
+            }
         }
         dataPackJSON.setFlag(1);
         dataPackJSON.setMsg("ERROR");
         return dataPackJSON;
 
-    }*/
-    //访问外部API
+    }
+    //访问外部API公共方法
     public  String visitThirdParties(MultiValueMap<String, String> params,String url){
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -61,25 +81,34 @@ public class BackstageController {
     }
     //获取验证码
     @RequestMapping("/getcode")
-    public DataPackJSON getCode(String phone){
+    public DataPackJSON getCode(String phone,HttpServletRequest request){
+        HttpSession session=request.getSession();
+        int code=(int)((Math.random()*9+1)*1000);
+        session.setAttribute("code",code);
         DataPackJSON dataPackJSON=new DataPackJSON();
-        //等待调用获取验证码API
-        return dataPackJSON;
-    }
-    //登录
-    @RequestMapping("/backstage")
-    public DataPackJSON backStage(String phone, String code, HttpServletRequest request){
-        DataPackJSON dataPackJSON=new DataPackJSON();
-        MultiValueMap<String, String> params= new LinkedMultiValueMap<>();
-        params.add("phone",phone);
-        params.add("verifyCode",code);
-        params.add("token","DLO3303A10XMDO39X0TYZQAP3LXDJ3X19283E2RXWIK");
-        String res=this.visitThirdParties(params,"https://console.yingpiao360.com/api/common/send-verify-code");
-        if(res!=null){
-            JSONObject data = (JSONObject) JSON.parse(res);
+        UserInfo userInfo=userInfoMapper.selectByPhone(phone);
+        if(userInfo!=null){
+            //发送验证码
+            MultiValueMap<String, String> params= new LinkedMultiValueMap<>();
+            params.add("phone",phone);
+            params.add("verifyCode",String.valueOf(code));
+            params.add("token","DLO3303A10XMDO39X0TYZQAP3LXDJ3X19283E2RXWIK");
+            String res=this.visitThirdParties(params,"https://console.yingpiao360.com/api/common/send-verify-code");
+            if(res!=null){
+                JSONObject data = (JSONObject) JSON.parse(res);
+                String res_data=data.getString("data");
+                if("true".equals(res_data)){
+                    dataPackJSON.setFlag(0);
+                    dataPackJSON.setMsg("验证码已发送");
+                    return dataPackJSON;
+                }
+            }
         }
+        dataPackJSON.setFlag(1);
+        dataPackJSON.setMsg("此用户不存在");
         return dataPackJSON;
     }
+
 
     @RequestMapping("/findAllUser")
     public DataPackJSON findAllUser(HttpServletRequest request){
